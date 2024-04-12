429f257259d4bde89d20924c812b8e127f0353c6
Julian Maximiliano González <Julian.gonzalez@cleveritgroup.com>
2024-04-12 14:16:38 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -1,25 +1,50 @@
-terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "3.4"
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket_web.bucket_regional_domain_name
+    origin_id   = var.bucket_name
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
   }
-}
-provider "aws" {
-  region     = "us-east-1"
-}
 
-resource "aws_s3_bucket" "bucket_web" {
-  bucket = var.bucket_name
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront for ${var.bucket_name}"
+  default_root_object = "index.html"
 
-  tags = {
-    Name = format("%s-web", var.bucket_name)
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.bucket_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
-  website {
-    index_document = "index.html"
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
 
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Origin access identity for ${var.bucket_name}"
+}
