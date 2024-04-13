a191dc0e15f5588b8428c21417adc72d1d994d66
Julian Maximiliano González <Julian.gonzalez@cleveritgroup.com>
2024-04-13 20:46:05 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -28,13 +28,20 @@ resource "aws_acm_certificate" "certificate" {
 }
 
 resource "aws_route53_record" "cert_dns" {
-  count           = length(aws_acm_certificate.certificate.domain_validation_options)
-  zone_id         = data.aws_route53_zone.certificate_route53_zone.zone_id
-  name            = aws_acm_certificate.certificate.domain_validation_options[count.index].resource_record_name
-  type            = aws_acm_certificate.certificate.domain_validation_options[count.index].resource_record_type
-  records         = [aws_acm_certificate.certificate.domain_validation_options[count.index].resource_record_value]
-  ttl             = 60
+  for_each = {
+    for robo in aws_acm_certificate.certificate.domain_validation_options : robo.domain_name => {
+      name   = robo.resource_record_name
+      record = robo.resource_record_value
+      type   = robo.resource_record_type
+    }
+  }
+
   allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.certificate_route53_zone.zone_id
 }
 
 resource "aws_acm_certificate_validation" "certificate" {
