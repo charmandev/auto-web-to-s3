2d8da7ff1338ae706aa04e5aa51ecd97545c9d64
Julian Maximiliano González <Julian.gonzalez@cleveritgroup.com>
2024-04-13 12:02:19 -0300

    update

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -80,71 +80,9 @@ resource "aws_acm_certificate_validation" "cert" {
 }
 
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.bucket_web.bucket_regional_domain_name
-    origin_id   = var.bucket_name
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
-    }
-  }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "CloudFront for ${var.bucket_name}"
-  default_root_object = "index.html"
 
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = var.bucket_name
 
-    forwarded_values {
-      query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  price_class = "PriceClass_100"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.cert.certificate_arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2018"
-  }
-}
-
-output "bucket_website_endpoint" {
-  description = "The website endpoint of the S3 bucket"
-  value       = aws_s3_bucket.bucket_web.website_endpoint
-}
-
-output "cloudfront_domain_name" {
-  description = "The domain name of the CloudFront distribution"
-  value       = aws_cloudfront_distribution.s3_distribution.domain_name
-}
-
-output "acm_certificate_arn" {
-  description = "The ARN of the ACM certificate"
-  value       = aws_acm_certificate.cert.arn
-}
-
-output "route53_record" {
-  description = "The DNS record for the domain"
-  value       = aws_route53_record.www.name
-}
--- aws-s3/output.tf
+++ aws-s3/output.tf
@@ -1,3 +1,24 @@
 output "site_url" {
+  description = "The website endpoint of the S3 bucket"
   value = aws_s3_bucket.bucket_web.website_endpoint
+}
+
+
+output "cloudfront_domain_name" {
+  description = "The domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "acm_certificate_arn" {
+  description = "The ARN of the ACM certificate"
+  value       = aws_acm_certificate.cert.arn
+}
+
+output "route53_record" {
+  description = "The DNS record for the domain"
+  value       = aws_route53_record.www.name
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Origin access identity for ${var.bucket_name}"
 }
