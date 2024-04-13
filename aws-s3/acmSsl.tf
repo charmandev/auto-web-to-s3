fb32153642a23f6c8bcff1b06219daa6c07025ba
Julian Maximiliano González <Julian.gonzalez@cleveritgroup.com>
2024-04-13 13:25:23 -0300

    a

--- aws-s3/acmSsl.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "cert_validation" {
-  triggers = {
-    certificate_arn = aws_acm_certificate.cert.arn
-  }
-
-  provisioner "local-exec" {
-    command = "sleep 30"
-  }
-}
--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -72,14 +72,12 @@ resource "aws_route53_record" "validation" {
 
 */
 resource "aws_route53_record" "validation" {
-  count   = length(aws_acm_certificate.cert.domain_validation_options)
-  name    = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_name, count.index)
-  type    = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_type, count.index)
+  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
   zone_id = aws_route53_zone.my_zone.zone_id
-  records = [element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_value, count.index)]
+  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
   ttl     = 60
 
-  depends_on = [null_resource.cert_validation]
 }
 
 resource "aws_acm_certificate_validation" "cert" {
