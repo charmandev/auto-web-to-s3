2b32da39cbd3de9d69a23089df50a373c3529fa5
jgonzalez-clever <julian.gonzalez@cleveritgroup.com>
2024-04-14 01:00:49 -0300

    update

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -43,35 +43,3 @@ resource "aws_s3_bucket" "bucket_web" {
 }
 
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "tuwebi.com.ar"
-  validation_method = "DNS"
-
-  tags = {
-    Name = "tuwebi.com.ar"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-  name    = each.value.name
-  type    = each.value.type
-  zone_id = aws_route53_zone.my_zone.zone_id
-  records = [each.value.record]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
-}
