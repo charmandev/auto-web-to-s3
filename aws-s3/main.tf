7d490560000028673627d84cd4a46ce8c8e5988d
Julian Maximiliano González <Julian.gonzalez@cleveritgroup.com>
2024-04-15 14:23:25 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -11,13 +11,6 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_route53_zone" "my_zone" {
-  name = "tuwebi.com.ar"
-
-  lifecycle {
-    ignore_changes = all
-  }
-}
 
 resource "aws_s3_bucket" "bucket_web" {
   bucket = var.bucket_name
@@ -31,57 +24,6 @@ resource "aws_s3_bucket" "bucket_web" {
   }
 }
 
-resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-  comment = "Origin access identity for ${var.bucket_name}"
-}
-
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "tuwebi.com.ar"
-  validation_method = "DNS"
-
-  tags = {
-    Name = "tuwebi.com.ar"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  name    = each.value.name
-  type    = each.value.type
-  zone_id = aws_route53_zone.selected.zone_id
-  records = [each.value.record]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
-}
-
-resource "aws_route53_record" "www" {
-  name    = "www.tuwebi.com.ar"
-  type    = "A"
-  zone_id = aws_route53_zone.selected.zone_id
-
-  alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
->>>>>>> abe62422c0cf1e30dd29d1f8ee37e41cf0f91a03
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.bucket_web.bucket_regional_domain_name
