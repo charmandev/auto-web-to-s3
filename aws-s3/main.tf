505e8bae45e31dfb43c9fa57a588d9f7db9e6447
julimax <69334886+julimax@users.noreply.github.com>
2024-04-15 13:58:44 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -123,20 +123,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 }
 
-output "bucket_website_endpoint" {
-  description = "The website endpoint of the S3 bucket"
-  value       = aws_s3_bucket.bucket_web.website_endpoint
-}
 
-output "cloudfront_domain_name" {
-  description = "The domain name of the CloudFront distribution"
-  value       = aws_cloudfront_distribution.s3_distribution.domain_name
-}
-
-output "acm_certificate_arn" {
-  description = "The ARN of the ACM certificate"
-  value       = aws_acm_certificate.cert.arn
-}
 
 output "route53_record" {
   description = "The DNS record for the domain"
