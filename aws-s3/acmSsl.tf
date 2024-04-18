024d5ce82b33d31b822ee81039341c8731259dc4
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-04-18 10:14:24 -0300

    Update acmSsl.tf

--- aws-s3/acmSsl.tf
+++ aws-s3/acmSsl.tf
@@ -23,24 +23,3 @@ resource "aws_acm_certificate" "certificate" {
   }
 }
 
-resource "aws_route53_record" "cert_dns" {
-  for_each = {
-    for robo in aws_acm_certificate.certificate.domain_validation_options : robo.domain_name => {
-      name   = robo.resource_record_name
-      record = robo.resource_record_value
-      type   = robo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.certificate_route53_zone.zone_id
-}
-
-resource "aws_acm_certificate_validation" "certificate" {
-  certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_dns : record.fqdn]
-}
