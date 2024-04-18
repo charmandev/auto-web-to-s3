672863eedab2fadf9244deb738616cf1a6556856
Julian Maximiliano González <Julian.gonzalez@cleveritgroup.com>
2024-04-18 18:32:02 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
     backend "s3" {
-    bucket = "my-terraform-state"
+    bucket = "my-terraform-state-webi"
     key    = "./terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-state"
