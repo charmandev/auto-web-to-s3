258be7f1335979fb28eaf5e64f01a837611acb78
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-04-18 10:21:08 -0300

    Update route53.tf

--- aws-s3/route53.tf
+++ aws-s3/route53.tf
@@ -1,3 +1,15 @@
+resource "aws_route53_record" "www" {
+  name    = "www.tuwebi.com.ar"
+  type    = "A"
+  zone_id = aws_route53_zone.my_zone.zone_id
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_route53_record" "cert_dns" {
   for_each = {
     for robo in aws_acm_certificate.certificate.domain_validation_options : robo.domain_name => {
