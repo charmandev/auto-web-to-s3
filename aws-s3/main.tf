f3c1683a2f894759942531e337814327d4846a58
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-04-18 10:20:58 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -19,17 +19,7 @@ resource "aws_route53_zone" "my_zone" {
   }
 }
 
-resource "aws_route53_record" "www" {
-  name    = "www.tuwebi.com.ar"
-  type    = "A"
-  zone_id = aws_route53_zone.my_zone.zone_id
 
-  alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
 resource "aws_s3_bucket" "bucket_web" {
   bucket = var.bucket_name
 
