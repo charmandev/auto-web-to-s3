62129db06cd08d479ccded2112511ff62e198ba9
Julian Maximiliano González <Julian.gonzalez@cleveritgroup.com>
2024-04-18 10:54:42 -0300

    Update acmSsl.tf

--- aws-s3/acmSsl.tf
+++ aws-s3/acmSsl.tf
@@ -2,20 +2,14 @@
 # * ACM *                           #
 # ********************************* # 
 
-
-variable "root_domain_name" {
-  type    = string
-  default = "tuwebi.com.ar"
-}
-
 data "aws_route53_zone" "certificate_route53_zone" {
-  name         = var.root_domain_name
+  name         = aws_route53_zone.name
   private_zone = false
 }
 
 resource "aws_acm_certificate" "certificate" {
-  domain_name               = var.root_domain_name
-  subject_alternative_names = ["*.${var.root_domain_name}"]
+  domain_name               = aws_route53_zone.name
+  subject_alternative_names = ["*.${aws_route53_zone.name}"]
   validation_method         = "EMAIL"
 
   lifecycle {
