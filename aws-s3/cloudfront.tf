f61ec55987dffbb4d48ba896582dabf1e0455eb7
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-04-23 01:05:44 -0300

    Update cloudfront.tf

--- aws-s3/cloudfront.tf
+++ aws-s3/cloudfront.tf
@@ -43,5 +43,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
   }
 
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.certificate.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
 }
-
