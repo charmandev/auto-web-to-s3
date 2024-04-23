cac99d2ef75e5cc841705f0d14ff3b781344736a
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-04-23 00:53:14 -0300

    Update output.tf

--- aws-s3/output.tf
+++ aws-s3/output.tf
@@ -8,7 +8,3 @@ output "cloudfront_domain_name" {
   value       = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
-output "acm_certificate_arn" {
-  description = "The ARN of the ACM certificate"
-  value       = aws_acm_certificate.certificate.arn
-}
