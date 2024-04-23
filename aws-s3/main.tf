ba3dffe1123ae71101a295cb1e7f29cb63674630
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-04-23 01:01:01 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -11,6 +11,25 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+resource "aws_route53_zone" "my_zone" {
+  name = "charmandev.com.ar"
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "aws_route53_record" "www" {
+  name    = "www.charmandev.com.ar"
+  type    = "A"
+  zone_id = aws_route53_zone.my_zone.zone_id
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
 resource "aws_s3_bucket" "bucket_web" {
   bucket = var.bucket_name
 
@@ -22,6 +41,5 @@ resource "aws_s3_bucket" "bucket_web" {
     index_document = "index.html"
   }
 }
-
 
 
