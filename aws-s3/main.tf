2a2735c249b05ea17509b67a3ed9c96121d1382f
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-04-26 16:11:44 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -38,3 +38,12 @@ resource "aws_s3_bucket" "bucket_web" {
 
 }
 
+resource "aws_s3_bucket_object" "bucket_objects" {
+  for_each = aws_s3_bucket.bucket_web.objects
+
+  bucket = aws_s3_bucket.bucket_web.bucket
+  key    = each.value.key
+
+  # Agrega dependencia para que los objetos se eliminen antes de recrear el bucket
+  depends_on = [aws_s3_bucket.bucket_web]
+}
