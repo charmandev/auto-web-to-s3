bf8865317f873318638ebe217d25ed4cf56a9cd5
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-04-26 01:49:39 -0300

    Update main.tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "tfstate-tar-testing-char"
-    dynamodb_table = "my-terraform-state-lock-aws-web-to-s3"
+    dynamodb_table = "my-terraform-state-lock-aws-web-to-s3-test"
     key            = "environments/testing/aws-s3-bucket.tfstate"
     region         = "us-east-1"
   }
