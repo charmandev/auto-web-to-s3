0402ccbfbe5f9010a4f1c1bb12238ec1649bdbea
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-11-21 15:56:39 -0300

    Update acmSsl.tf

--- aws-s3/acmSsl.tf
+++ aws-s3/acmSsl.tf
@@ -9,10 +9,7 @@ data "aws_route53_zone" "certificate_route53_zone" {
 
 resource "aws_acm_certificate" "certificate" {
   domain_name               = aws_route53_zone.my_zone.name
-  subject_alternative_names = [
-    "*.${aws_route53_zone.my_zone.name}",     # Todos los subdominios de primer nivel
-    "www.dev-${aws_route53_zone.my_zone.name}" # Subdominio de segundo nivel específico
-  ]
+  subject_alternative_names = ["*.${aws_route53_zone.my_zone.name}"]
   validation_method         = "DNS"
 
   lifecycle {
