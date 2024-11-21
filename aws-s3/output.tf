7549776382fef8b39be2c11982e4d91793eb7aa2
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-11-21 17:53:25 -0300

    Update output.tf

--- aws-s3/output.tf
+++ aws-s3/output.tf
@@ -3,9 +3,19 @@ output "site_url" {
   value = aws_s3_bucket.bucket_web.website_endpoint
 }
 
+output "site_url_dev" {
+  description = "The website endpoint of the S3 bucket"
+  value = aws_s3_bucket.bucket_web_dev.website_endpoint
+}
+
 output "cloudfront_domain_name" {
   description = "The domain name of the CloudFront distribution"
   value       = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "cloudfront_domain_name_dev" {
+  description = "The domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.s3_distribution_dev.domain_name
 }
 
 output "acm_certificate_arn" {
