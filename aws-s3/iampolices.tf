2b49e10eee692d4a0b469ae518ee7d33bd2dba69
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-11-21 18:09:37 -0300

    Update iampolices.tf

--- aws-s3/iampolices.tf
+++ aws-s3/iampolices.tf
@@ -35,3 +35,41 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+
+resource "aws_s3_bucket_policy" "bucket_policy_dev" {
+  depends_on = [aws_s3_bucket_public_access_block.public_access_block, aws_s3_bucket_object.site_files]
+  bucket = aws_s3_bucket.bucket_web_dev.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = [
+          "s3:GetObject"
+        ],
+        Resource = "${aws_s3_bucket.bucket_web_dev.arn}/*"
+      },
+      {
+        Sid = "PublicListBucket"
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:ListBucket"
+        ],
+        Resource = "${aws_s3_bucket.bucket_web_dev.arn}"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket_web_dev.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
