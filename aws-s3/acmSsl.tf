633b138174642594330ee2e052a485badb8b488b
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-11-23 12:23:15 -0300

    Update acmSsl.tf

--- aws-s3/acmSsl.tf
+++ aws-s3/acmSsl.tf
@@ -2,16 +2,19 @@
 # * ACM *                           #
 # ********************************* # 
 
+# Usar la hosted zone existente
 data "aws_route53_zone" "certificate_route53_zone" {
-  name         = aws_route53_zone.my_zone.name
+  name         = var.DOMINIO
   private_zone = false
 }
 
+# Buscar un certificado existente
 data "aws_acm_certificate" "existing_certificate" {
-  domain   = aws_route53_zone.my_zone.name
+  domain   = var.DOMINIO
   statuses = ["ISSUED"]  # Solo selecciona certificados válidos
 }
 
+# Exportar el ARN del certificado existente
 output "certificate_arn" {
   value = data.aws_acm_certificate.existing_certificate.arn
 }
