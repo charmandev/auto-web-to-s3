54de04fea5c71551a6a1bdc5a069604e8deee04f
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-11-23 11:56:28 -0300

    Update acmSsl.tf

--- aws-s3/acmSsl.tf
+++ aws-s3/acmSsl.tf
@@ -7,7 +7,7 @@ data "aws_route53_zone" "certificate_route53_zone" {
   private_zone = false
 }
 
-resource "aws_acm_certificate" "certificate" {
+data "aws_acm_certificate" "certificate" {
   domain_name               = aws_route53_zone.my_zone.name
   subject_alternative_names = ["*.${aws_route53_zone.my_zone.name}"]
   validation_method         = "DNS"
