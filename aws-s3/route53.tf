21d5441f7cc40afe04b0925f112dff255c128703
julian gonzalez <69334886+julimax@users.noreply.github.com>
2024-11-23 12:45:39 -0300

    Update route53.tf

--- aws-s3/route53.tf
+++ aws-s3/route53.tf
@@ -42,21 +42,3 @@ resource "aws_route53_record" "dev" {
     evaluate_target_health = false
   }
 }
-
-# Crear los registros para la validación del certificado (si es necesario)
-resource "aws_route53_record" "cert_dns" {
-  for_each = {
-    for dvo in data.aws_acm_certificate.existing_certificate.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.my_zone.zone_id
-}
